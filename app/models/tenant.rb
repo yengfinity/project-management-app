77271f524178f6b207fce0fc62be300f54f8e717
Yeng <leeyeng2005@gmail.com>
2017-07-09 10:42:29 -0500

    Add plans with Milia

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,17 +1,19 @@
 class Tenant < ActiveRecord::Base
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+  validates_uniqueness_of :name
+  validates_presence_of :name
 
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
-      tenant = Tenant.new(:name => tenant_params[:name])
+      tenant = Tenant.new(tenant_params)
 
       if new_signups_not_permitted?(coupon_params)
 
-        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
+        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time"
 
-      else 
+      else
         tenant.save    # create the tenant
       end
       return tenant
@@ -43,5 +45,5 @@ class Tenant < ActiveRecord::Base
       #
     end
 
-   
+
 end
