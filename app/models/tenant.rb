013555f92e40f086a393955e9667840576087343
Yeng <leeyeng2005@gmail.com>
2017-07-09 18:05:58 -0500

    create project show and list

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -6,19 +6,23 @@ class Tenant < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
 
-    def self.create_new_tenant(tenant_params, user_params, coupon_params)
+  def can_create_projects?
+    (plan == 'free' && projects.count < 1) || (plan == 'premium')
+  end
 
-      tenant = Tenant.new(tenant_params)
+  def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
-      if new_signups_not_permitted?(coupon_params)
+    tenant = Tenant.new(tenant_params)
 
-        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time"
+    if new_signups_not_permitted?(coupon_params)
 
-      else
-        tenant.save    # create the tenant
-      end
-      return tenant
+      raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time"
+
+    else
+      tenant.save    # create the tenant
     end
+    return tenant
+  end
 
   # ------------------------------------------------------------------------
   # new_signups_not_permitted? -- returns true if no further signups allowed
