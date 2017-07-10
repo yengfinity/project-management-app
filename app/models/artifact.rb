c4fff17827019a69629e4d26dd3fa47edd6783f4
Yeng <leeyeng2005@gmail.com>
2017-07-09 20:10:08 -0500

    Set up artifacts

---
+++ app/models/artifact.rb
@@ -0,0 +1,18 @@
+class Artifact < ActiveRecord::Base
+  attr_accessor :upload
+  belongs_to :project
+
+  MAX_FILESIZE = 10.megabytes
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+
+  validate :upload_file_size
+
+  private
+
+  def upload_file_size
+    if upload
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
+end
