00cc71e28692a8cc81f8e5bb30201b40bf182199
Yeng <leeyeng2005@gmail.com>
2017-07-11 17:45:18 -0500

    Add update tentant plan

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,7 @@ Rails.application.routes.draw do
     :passwords => "milia/passwords",
   }
 
+  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
+
 end
